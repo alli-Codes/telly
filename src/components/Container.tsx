6d238be78a884143dc796da7fb1f5ed0ffaae7dc
= <=>
2024-01-03 20:59:00 +0100

    Created the Container Component

---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import SideBar from "./SideBar"
+
+const Container = function () {
+    return (
+        <>
+            <div className="bg-gray">
+                <SideBar />
+            </div>
+        </>
+    )
+}
+
+export default Container
