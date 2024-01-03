b76f3dd97fbc0e6264c66eae08e87de63e7e60a3
= <=>
2024-01-03 16:06:48 +0100

    Setting up Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   const name = 'Evans';
   if (name) 
-    return <h1>Hello {name}</h1>
+    return <h1 className="text-blue-300">Hello {name}</h1>
   return <h1>Hello World</h1>
 }
 
