29a4ca12191376e6bf75fab65c34204ce80fa56f
= <=>
2024-01-04 15:53:24 +0100

    Making the active indicator

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,20 +5,20 @@ import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined';
 
 const SideBar = function () {
     return (
-        <div className="h-[100vh] max-w-56 px-4 py-16 flex flex-col gap-20 bg-white  rounded">
+        <div className="h-[100vh] max-w-56 py-16 flex flex-col gap-20 bg-white  rounded">
             <div className='flex items-center gap-2'>
                 <MenuIcon fontSize="large" />
                 <p>Telly</p>
             </div>
 
             <nav className='flex flex-col gap-6'>
-                <a href="#" className='flex items-center gap-4 active'>
+                <a href="#" className='px-4 flex items-center gap-4 active__nav-link relative'>
                     <HomeOutlinedIcon sx={{ fontSize:30 }} /> <p>Home</p>
                 </a>
-                <a href="#" className='flex items-center gap-4'>
+                <a href="#" className='px-4 flex items-center gap-4 relative'>
                     <VideocamOutlinedIcon sx={{ fontSize:30 }} /> <p>Movies</p>
                 </a>
-                <a href="#" className='flex items-center gap-4'>
+                <a href="#" className='px-4 flex items-center gap-4 relative'>
                     <LiveTvOutlinedIcon sx={{ fontSize:28 }} /> <p>Tv Series/ Shows</p>
                 </a>
             </nav>
